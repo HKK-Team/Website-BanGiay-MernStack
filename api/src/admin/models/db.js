ea16f0056710be606f20b1f2a5a3cbcb4c87254e
MinhKhanh693 <58809781+MinhKhanh693@users.noreply.github.com>
2021-09-07 13:12:34 +0700

    move database server to singapo

--- api/src/admin/models/db.js
+++ api/src/admin/models/db.js
@@ -1,12 +1,12 @@
-var mongoose = require('mongoose');
-var url = "mongodb+srv://khanhzu:mARUjKuNSVoTJziV@newcluster.klghj.mongodb.net/online_shop_project?retryWrites=true&w=majority";
+var mongoose = require("mongoose");
+var url =
+  "mongodb+srv://minhkhanh:020901@cluster0.zuhc5.mongodb.net/WebsiteBanGiay?retryWrites=true&w=majority";
 
-mongoose.connect(url,(err) =>{
-    if(!err){
-        console.log("Connected Mongodb Successfully!");
-    }
-    else{
-        console.log("Error in DB Connection : " + err);
-    }
+mongoose.connect(url, (err) => {
+  if (!err) {
+    console.log("Connected Mongodb Successfully!");
+  } else {
+    console.log("Error in DB Connection : " + err);
+  }
 });
-require('./admin.model');
+require("./admin.model");
