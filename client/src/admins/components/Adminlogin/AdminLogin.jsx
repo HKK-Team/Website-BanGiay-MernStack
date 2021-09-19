949b20b370c906a79ce941471cf341d38a25cb35
MinhKhanh693 <58809781+MinhKhanh693@users.noreply.github.com>
2021-09-19 09:09:36 +0700

    Admin login page

---
+++ client/src/admins/components/Adminlogin/AdminLogin.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AdminLogin.css";
+
+export default function AdminLogin() {
+  return (
+    <div className="AdminLogin_box">
+      <form action="">
+        <h1>Admin Login</h1>
+        <input type="Email" placeholder="Nhập Email của bạn" />
+        <input type="password" placeholder="Nhập mật khẩu của bạn" />
+        <button>Đăng nhập</button>
+      </form>
+      <h2>Trang quản trị <br/>copyright © 2021 HKK team</h2>
+    </div>
+  );
+}
